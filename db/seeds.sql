079044461c2a3299072b0e09c6d63b353ab51cf1
Dominique Christopher <showlandproductions@gmail.com>
2021-02-16 13:39:12 -0800

    Added Seeds.sql with Burger Options

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers_db.burgers (burger_name, devoured)
+VALUES
+("Chilli Cheesburger", false),
+("Bison Avocado Burger", false),
+("Vegan  Burger", false),
+("Breakfast Burger", false),
+("Dessert Burger", false);
