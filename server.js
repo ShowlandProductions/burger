5d6363d1f78f6171351c39711007f18e6b00e2f8
Dominique Christopher <showlandproductions@gmail.com>
2021-02-16 17:55:17 -0800

    Made sure app was working before Heroku Deploy

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
-
 const PORT = process.env.PORT || 8080;
-
 const app = express();
+app.use(express.static('public'));
+// Parse app
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Set Handlebars.
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+// Import routes & give the server access to them.
+const routes = require('./controllers/burgers_controller.js');
+app.use(routes);
+// Start server function
+app.listen(PORT, function() {
+// Log when server starts
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
